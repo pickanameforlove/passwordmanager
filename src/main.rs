44aee7aaaa67ad7a765f9c0ad14b756021e44886
lmk <3084389453@qq.com>
2021-09-20 19:14:11 +0800

    error handling

--- src/main.rs
+++ src/main.rs
@@ -71,18 +71,27 @@ fn add_password(){
     let url = "mysql://root:123456@localhost:3306/passworddb";
     let opts = Opts::from_url(url).expect("mysql initialize Error!");
 
-    let pool = Pool::new(opts).unwrap();
+    let pool = match  Pool::new(opts){
+        Ok(p) => p,
+        Err(error) => panic!("Problem settting the pool: {:?}", error),
+    };
 
-    let mut conn = pool.get_conn().unwrap();
+    let mut conn = match pool.get_conn(){
+        Ok(c) => c,
+        Err(error) => panic!("Problem getting the connection: {:?}", error),
+    };
 
-    conn.exec_drop(
+    match conn.exec_drop(
         "INSERT INTO passwords (description, user, password)
          VALUES (:des, :u, :pass)", params! {
              "des" => &website,
              "u" => &username,
              "pass" => &pass,
          }
-    );
+    ) {
+        Ok(_) => (),
+        Err(error) => panic!("Problem execucing the statement: {:?}", error),
+    };
 
     let pass_sec = pass.clone();
     let clipboard =  SystemClipboard::new().unwrap();
