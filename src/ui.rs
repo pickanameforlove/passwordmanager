a5e63d8bfcb1d0bcb125aa334b8c3a513d3e557a
lmk <3084389453@qq.com>
2021-09-21 12:57:47 +0800

    perfect view by using the offcial crate

--- src/ui.rs
+++ src/ui.rs
@@ -9,6 +9,7 @@ use std::thread;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::cell::RefCell;
+mod p;
 
 
 pub struct password{
@@ -52,11 +53,13 @@ impl epi::App for MyApp {
 
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            egui::Grid::new("some_unique_id").show(ui, |ui| {
+            
+            egui::Grid::new("some_unique_id").min_col_width(180.0).show(ui, |ui| {
+                // ui.set_min_width(200.0);
                 ui.label("description");
                 ui.label("user");
                 ui.label("password");
-                ui.label("option");
+
                 ui.end_row();
                 
                 let mut vec_iter = self.data.iter();
@@ -65,24 +68,26 @@ impl epi::App for MyApp {
                     // let (tx,rx) = mpsc::channel();
                     ui.label(&password.description);
                     ui.label(&password.user);
-                    if ui.add(egui::Button::new("copy password").frame(false)).clicked() {
-                                        let pass_sec = (&password.password).clone();
-                                        let clipboard =  SystemClipboard::new().unwrap();
-                                        clipboard.set_string_contents(pass_sec).unwrap();
-                    };
+                    let mut p_clone = password.password.clone();
+                    ui.add(p::password(&mut p_clone));
+                    // if ui.add(egui::Button::new("copy password").frame(false)).clicked() {
+                    //                     let pass_sec = (&password.password).clone();
+                    //                     let clipboard =  SystemClipboard::new().unwrap();
+                    //                     clipboard.set_string_contents(pass_sec).unwrap();
+                    // };
                                        
-                    if ui.add(egui::Button::new("show password").frame(false)).clicked(){
-                        let data = (&password.password).clone();
+                    // if ui.add(egui::Button::new("show password").frame(false)).clicked(){
+                    //     let data = (&password.password).clone();
 
-                        let pasd_clone = Arc::clone(&self.pasd);
-                        // let change_value = thread::spawn(move || {
-                        //     let mut d = pasd_clone.lock().unwrap();
-                        //     *d = data;
-                        // });
-                        let mut d = pasd_clone.lock().unwrap();
-                        *d = data;
-                        // tx.send(data).unwrap();
-                    };
+                    //     let pasd_clone = Arc::clone(&self.pasd);
+                    //     // let change_value = thread::spawn(move || {
+                    //     //     let mut d = pasd_clone.lock().unwrap();
+                    //     //     *d = data;
+                    //     // });
+                    //     let mut d = pasd_clone.lock().unwrap();
+                    //     *d = data;
+                    //     // tx.send(data).unwrap();
+                    // };
                     
                     // match rx.try_recv(){
                     //     Ok(value) => {
@@ -90,12 +95,28 @@ impl epi::App for MyApp {
                     //     },
                     //     Err(e) => println!("this a error"),
                     // };
-                    let content = (*self.pasd.lock().unwrap()).clone();
-                    ui.label( content);    
+                    // let content = (*self.pasd.lock().unwrap()).clone();
+                    // ui.label( content);   
+                    // if ui.add(egui::Button::new("show password plus").frame(false)).clicked() {
+                    //     self.showpassword();
+                    // }
+                    
                     ui.end_row();
                 }
             });
 
+        });
+    }
+}
+
+impl MyApp {
+    fn showpassword(&self) {
+        let mut ctx = egui::CtxRef::default();
+        let raw_input = egui::RawInput::default();
+        ctx.begin_frame(raw_input);
+        let mut open = true;
+        egui::Window::new("show").open(&mut open).show(&ctx,|ui|{
+            ui.label("dsaeg");
         });
     }
 }
